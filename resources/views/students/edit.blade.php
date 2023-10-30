094bab4bc987e4a9537bad70ba40534faf0962b2
tony2928 <tonytr91@gmail.com>
2023-10-30 02:27:52 -0600

    updated

--- resources/views/students/edit.blade.php
+++ resources/views/students/edit.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
                     <form method="POST" action="{{ route('students.update') }}">
                         @csrf
                         <input type="hidden" name="id" value="{{$student->id}}">
